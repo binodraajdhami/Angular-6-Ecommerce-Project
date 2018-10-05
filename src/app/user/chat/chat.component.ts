99d47e83307bae99ca896153cb3656af51718e72
jskisor <kisorgiri@gmail.com>
2018-10-05 09:25:00 +0545

    chat complete

--- src/app/user/chat/chat.component.ts
+++ src/app/user/chat/chat.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { SocketService } from '../../shared/services/socket.service';
+import { MsgService } from '../../shared/services/msg.service';
 
 @Component({
   selector: 'app-chat',
@@ -7,33 +8,76 @@ import { SocketService } from '../../shared/services/socket.service';
   styleUrls: ['./chat.component.css']
 })
 export class ChatComponent implements OnInit {
-  public msg;
+  public msgBody: any;
   public user;
   public users;
-  constructor(public socketService: SocketService) { }
+  constructor(public socketService: SocketService,
+    public msgService: MsgService) { }
   public messages = [];
+  public userSelected: boolean = false;
+  public isTyping: boolean = false;
+  public nameaaa;
+
   ngOnInit() {
     this.user = JSON.parse(localStorage.getItem("user"));
     this.runSocket();
     this.socketService.socket.emit('user', { name: this.user.username });
+    this.msgBody = {
+      msg: '',
+      sender: '',
+      receiver: '',
+      receiverName: ''
+    }
   }
 
   runSocket() {
     console.log('i ma here at run socket');
     this.socketService.socket.on('replyMsg', (data) => {
       this.messages.push(data);
+      this.userSelected = true;
+      this.msgBody.receiver = data.senderId;
       console.log('data in client at hello event ', data);
     });
     this.socketService.socket.on('users', (data) => {
-      console.log('data', data);
       this.users = data;
     });
+    this.socketService.socket.on('is-typing', (data) => {
+      console.log('data.sender', data.sender);
+      this.nameaaa = data.sender
+      this.isTyping = true;
+    });
+    this.socketService.socket.on('typing-stopped', (data) => {
+      console.log('data.sender', data.sender);
+      this.nameaaa = data.sender;
+
+      this.isTyping = false;
+    })
   }
 
   send() {
-    this.socketService.socket.emit('newMsg', { msg: this.msg });
-    this.msg = '';
+    if (!this.userSelected) {
+      this.msgService.showInfo("Please select user to send msg");
+      return;
+    }
+    this.msgBody.sender = this.user.username;
+    this.socketService.socket.emit('newMsg', this.msgBody);
+    this.msgBody.msg = '';
   }
+
+  selectUser(user) {
+    this.userSelected = true;
+    this.msgBody.receiver = user.id
+    this.msgBody.receiverName = user.name;
+  }
+
+  typing(val) {
+    if (val) {
+      this.socketService.socket.emit('typing', this.msgBody);
+    } else {
+      this.socketService.socket.emit('typing-stop', this.msgBody);
+    }
+  }
+
 
 
 }
